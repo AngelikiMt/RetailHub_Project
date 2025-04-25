63a74ab98bb8d664a82f6847d7e9548f9c0889b8
AngelikiMt <matta.d.angeliki@gmail.com>
2025-04-25 10:51:14 +0300

    Includes comments editing

--- Includes.java
+++ Includes.java
@@ -1,11 +1,9 @@
-
-/* Includes holds a row from the INCLUDES table.
- * That is, which product was sold, in which transaction and how much quantity was sold. */
+// Which product was purchased, in which transaction and how many pieces were sold
 
 public class Includes {
-	private long transactionId; // In which transaction was the purchase made
-	private long productId; // Which product was purchased
-	private int soldQuantity; // How many pieces were sold
+	private long transactionId;
+	private long productId;
+	private int soldQuantity;
 
 	// Constructor
 	public Includes(long transactionId, long productId, int soldQuantity) {
@@ -27,7 +25,6 @@ public class Includes {
 		return soldQuantity;
 	}
 	
-	
 	// Set/Update values
 	public void setTransactionId(long transactionId) {
 		this.transactionId = transactionId;
@@ -40,8 +37,7 @@ public class Includes {
 	public void setSoldQuantity(int soldQuantity) {
 		this.soldQuantity = soldQuantity;
 	}
-	
-	// This give a nicer and cleaner look to the prints
+
 	@Override
 	public String toString() {
 		return "Includes: Transaction: " + transactionId + " | Product: " + productId + " | sold quantity: " + soldQuantity;
