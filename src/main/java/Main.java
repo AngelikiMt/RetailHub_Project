841e1aa3d1ce2bea103b6a3dfa7ee899ef717087
a.kesisi <esd25018@uom.edu.gr>
2025-05-23 23:51:45 +0300

    project directory changes

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+
+import java.util.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+public class Main {
+
+    //private static final Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+ 
+        new LoginFrame();
+    }
+}
+    
+
+
