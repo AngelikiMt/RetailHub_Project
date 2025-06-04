0dbaf2714b77bf5633a0ff56462ce3c662568738
AngelikiMt <matta.d.angeliki@gmail.com>
2025-06-04 15:23:15 +0300

    databaseConnector update

--- src/main/java/DatabaseConnector.java
+++ src/main/java/DatabaseConnector.java
@@ -1,19 +1,34 @@
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DatabaseConnector {
 
-   private static final String URL = "jdbc:mysql://164.92.229.54:3306/retailhub_db"; 
-    private static final String USER = "retailhub"; 
-    private static final String PASSWORD = "oMada3###ftw"; 
+    private static final String PROPERTIES_FILE = "config.properties"; 
+    private static String dbUrl;
+    private static String dbUser;
+    private static String dbPass;
+
+    static {
+        try (InputStream input = DatabaseConnector.class.getClassLoader().getResourceAsStream(PROPERTIES_FILE)) {
+            Properties props = new Properties();
+            if (input != null) {
+                props.load(input);
+                dbUrl = props.getProperty("db.url");
+                dbUser = props.getProperty("db.username");
+                dbPass = props.getProperty("db.password");
+            } else {
+                throw new RuntimeException("Could not find " + PROPERTIES_FILE);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static Connection getConnection() throws SQLException {
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException ex) {
-            ex.printStackTrace();
-        }
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+        return DriverManager.getConnection(dbUrl, dbUser, dbPass);
     }
 }
