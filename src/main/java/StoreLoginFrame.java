d0e0535227804d5456fac12bac581b2b6ecb457f
AngelikiMt <matta.d.angeliki@gmail.com>
2025-06-05 09:58:01 +0300

    create storeLoginFrame

--- src/main/java/StoreLoginFrame.java
+++ src/main/java/StoreLoginFrame.java
@@ -3,6 +3,7 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Image;
 import java.awt.Insets;
 
 import javax.swing.ImageIcon;
@@ -13,55 +14,46 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
-import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.SwingUtilities;
 
-public class LoginFrame extends JFrame {
-    private static final String USERNAME = "";
-    private static final String PASSWORD = "";
+public class StoreLoginFrame extends JFrame {
+    private static final String PASSWORD = "12345";
 
-    public LoginFrame() {
-        setTitle("Login - Retail Hub");
+
+    public StoreLoginFrame() {
+        setTitle("Login - Store");
+        setFont(new Font("MinionPro", Font.BOLD, 20));
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         //setExtendedState(JFrame.MAXIMIZED_BOTH); // Fullscreen
                
-        ImageIcon icon = new ImageIcon(getClass().getResource("loginicon_120.png"));
-        JLabel logoLabel = new JLabel(icon);
+        ImageIcon icon = new ImageIcon(getClass().getResource("storeicon.png"));
+        // Get the scaled instance. This returns an 'Image' object.
+        Image scaledImage = icon.getImage().getScaledInstance(200, 200, Image.SCALE_SMOOTH); // Made it 150x150 for better visibility as a logo
+        // Create a new ImageIcon from the scaled Image
+        ImageIcon finalScaledIcon = new ImageIcon(scaledImage);        
+        JLabel logoLabel = new JLabel(finalScaledIcon);
         logoLabel.setHorizontalAlignment(SwingConstants.CENTER);
-        
-        // // === Custom JPanel with background image ===
-        // JPanel backgroundPanel = new JPanel() {
-        //     Image background = new ImageIcon(getClass().getResource("loginicon.png")).getImage();
-        //     @Override
-        //     protected void paintComponent(Graphics g) {
-        //         super.paintComponent(g);
-        //         g.drawImage(background, 10, 10, getWidth(), getHeight(), this);
-        //     }
-        // };
-        // backgroundPanel.setLayout(new GridBagLayout()); // Centers the login panel
 
         // === Connection Panel ===
         JPanel loginPanel = new JPanel(new GridBagLayout());
-        loginPanel.setOpaque(false); // Show the background behind it
+        loginPanel.setOpaque(false);
         
         GridBagConstraints gbc = new GridBagConstraints();
-        gbc.insets = new Insets(10, 10, 10, 10);
+        gbc.insets = new Insets(1, 10, 1, 10);
         gbc.fill = GridBagConstraints.HORIZONTAL;
 
-        JLabel userLabel = new JLabel("Username:");
-        JTextField userField = new JTextField(15);
-
         JLabel passLabel = new JLabel("Password:");
+        passLabel.setFont(new Font("MinionPro", Font.BOLD, 20));
         JPasswordField passField = new JPasswordField(15);
 
         JCheckBox showPass = new JCheckBox("Show Password");
-        showPass.setOpaque(false); // To be transparent
+        showPass.setFont(new Font("MinionPro", Font.BOLD, 17));
+        showPass.setOpaque(false);
         showPass.addActionListener(e -> {
             passField.setEchoChar(showPass.isSelected() ? (char) 0 : '•');
         });
 
-        //ImageIcon login = new ImageIcon(getClass().getResource("login.png"));
         JButton loginBtn = new JButton("Login");
         loginBtn.setBackground(new Color(128, 0, 128)); // Purple background
         loginBtn.setForeground(Color.WHITE);
@@ -69,13 +61,12 @@ public class LoginFrame extends JFrame {
         loginBtn.setPreferredSize(new Dimension(120, 30));
 
         loginBtn.addActionListener(e -> {
-            String enteredUser = userField.getText();
             String enteredPass = new String(passField.getPassword());
 
-            if (USERNAME.equals(enteredUser) && PASSWORD.equals(enteredPass)) {
+            if (PASSWORD.trim().equals(enteredPass)) {
                 SwingUtilities.invokeLater(() -> {
                     dispose();
-                    new MainMenu();
+                    new StoreFrame();
                 });
             } else {
                 JOptionPane.showMessageDialog(this, "Invalid credentials", "Error", JOptionPane.ERROR_MESSAGE);
@@ -84,19 +75,14 @@ public class LoginFrame extends JFrame {
 
         // === Add items ===
         gbc.gridx = 0; gbc.gridy = 0;
-        loginPanel.add(userLabel, gbc);
-        gbc.gridx = 1;
-        loginPanel.add(userField, gbc);
-
-        gbc.gridx = 0; gbc.gridy = 1;
         loginPanel.add(passLabel, gbc);
         gbc.gridx = 1;
         loginPanel.add(passField, gbc);
 
-        gbc.gridx = 1; gbc.gridy = 2;
+        gbc.gridx = 1; gbc.gridy = 1;
         loginPanel.add(showPass, gbc);
 
-        gbc.gridx = 1; gbc.gridy = 3;
+        gbc.gridx = 1; gbc.gridy = 2;
         loginPanel.add(loginBtn, gbc);
 
         JPanel mainPanel = new JPanel();
@@ -111,15 +97,10 @@ public class LoginFrame extends JFrame {
         panelGbc.gridx = 0; panelGbc.gridy = 1;
         mainPanel.add(loginPanel,panelGbc);
 
-        //panelGbc.weighty = 1.0;
-        //panelGbc.anchor = GridBagConstraints.PAGE_END;
-        //panelGbc.insets = new Insets(0, 0, 10, 0);
-        //backgroundPanel.add(loginPanel, panelGbc);
-        //setContentPane(backgroundPanel);
         this.getContentPane().add(mainPanel);
         getContentPane().setBackground(Color.WHITE);
         pack();
-        this.setSize(350,350);
+        this.setSize(370,350);
         this.setLocationRelativeTo(null);
         setVisible(true);
     }
